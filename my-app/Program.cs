5e25f6888fb800dce11a946724e4d988a076ff92
AchrafTh <Ach.Thamri@gmail.com>
2021-01-16 14:29:19 +0100

    .net Framework

---
+++ my-app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Achraf!");
+            Console.ReadKey(); //Make the console lasts
+        }
+    }
+}
